9b613a5cd2a0f7e9673c4784f6c76f43e5656a7e
qliu7715 <qliu7715@uni.sydney.edu.au>
2023-10-23 01:45:16 +1100

    Added LoginScreen

---
+++ src/components/LoginScreen.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="login">
+      <h2>Login</h2>
+      <input v-model="username" placeholder="Username" />
+      <input type="password" v-model="password" placeholder="Password" />
+      <button @click="handleLogin">Login</button>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        username: '',
+        password: ''
+      };
+    },
+    methods: {
+      handleLogin() {
+        if (this.username && this.password) {
+          // 这里可以进行登录逻辑，例如发送请求到后端验证用户名和密码
+          console.log('Logging in with', this.username, this.password);
+        } else {
+          alert('Please fill in both fields.');
+        }
+      }
+    }
+  };
+</script>
+  
+  <style scoped>
+  .login {
+    /* 这里可以添加样式 */
+  }
+  </style>
+  
